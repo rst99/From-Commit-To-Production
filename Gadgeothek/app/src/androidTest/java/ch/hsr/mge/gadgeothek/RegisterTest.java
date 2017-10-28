68e4712e60f0734a62ecc1774a89e70cc1a54ddc
rst99 <32651658+rst99@users.noreply.github.com>
2017-10-28 09:00:33 +0200

    E2E Test Update

--- Gadgeothek/app/src/androidTest/java/ch/hsr/mge/gadgeothek/RegisterTest.java
+++ Gadgeothek/app/src/androidTest/java/ch/hsr/mge/gadgeothek/RegisterTest.java
@@ -11,6 +11,9 @@ import org.junit.runner.RunWith;
 import ch.hsr.mge.gadgeothek.ui.RegisterActivity;
 
 import static android.support.test.espresso.Espresso.onView;
+import static android.support.test.espresso.action.ViewActions.click;
+import static android.support.test.espresso.action.ViewActions.closeSoftKeyboard;
+import static android.support.test.espresso.action.ViewActions.typeText;
 import static android.support.test.espresso.assertion.ViewAssertions.matches;
 import static android.support.test.espresso.matcher.ViewMatchers.isDisplayed;
 import static android.support.test.espresso.matcher.ViewMatchers.withId;
@@ -28,6 +31,13 @@ public class RegisterTest {
     @Test
     public void showsRegisterActivity()
     {
+
+        onView(withId(R.id.name)).perform(typeText("Reto Stahel"));
+        onView(withId(R.id.email)).perform(typeText("reto@stahel.com"));
+        onView(withId(R.id.matrikelnr)).perform(typeText("123445"));
+        onView(withId(R.id.password)).perform(typeText("12345"));
+        onView(withId(R.id.password)).perform(closeSoftKeyboard());
         onView(withId(R.id.registerButton)).check(matches(isDisplayed()));
+        onView(withId(R.id.registerButton)).perform(click());
     }
 }
